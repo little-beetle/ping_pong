856bf8f4937ca90140eadc349f6efb2807295620
Vladimir Kovalchuk <vvkovalchuk1409@gmail.com>
2022-02-07 20:46:46 +0200

    rackets

--- ping_pong.py
+++ ping_pong.py
@@ -29,4 +29,50 @@ for i in range(25):
         border.down()
 
 border.hideturtle()
+
+rocket_a = turtle.Turtle()
+rocket_a.color("white")
+rocket_a.shape("square")
+rocket_a.shapesize(stretch_wid=5, stretch_len=1)
+rocket_a.penup()
+rocket_a.goto(-450, 0)
+
+rocket_b = turtle.Turtle()
+rocket_b.speed(3)
+rocket_b.color("white")
+rocket_b.shape("square")
+rocket_b.shapesize(stretch_wid=5, stretch_len=1)
+rocket_b.penup()
+rocket_b.goto(450, 0)
+
+def move_up():
+    y = rocket_a.ycor() + 10
+    if y > 250:
+        y = 250
+    rocket_a.sety(y)
+
+def move_down():
+    y = rocket_a.ycor() - 10
+    if y < -250:
+        y = -250
+    rocket_a.sety(y)
+
+def move_up_b():
+    y = rocket_b.ycor() + 10
+    if y > 250:
+        y = 250
+    rocket_b.sety(y)
+
+def move_down_b():
+    y = rocket_b.ycor() - 10
+    if y < -250:
+        y = -250
+    rocket_b.sety(y)
+
+window.listen()
+window.onkeypress(move_up, "w")
+window.onkeypress(move_down, "s")
+window.onkeypress(move_up_b, "Up")
+window.onkeypress(move_down_b, "Down")
+
 window.mainloop()
